1bcde1eaf2a37a9a46b68ad113724f376202c8f7
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-02-28 08:02:52 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 31, 24, 166),
+                Color.fromARGB(255, 22, 110, 243),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
