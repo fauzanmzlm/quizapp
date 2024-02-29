44d3916b3e1a4dd08eef47aee393986fc6e03017
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-02-29 08:16:04 +0800

    commit

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,8 +12,8 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(135, 224, 224, 224),
           ),
-          const SizedBox(height: 80),
           const Text(
             'Learn flutter the fun way!',
             style: TextStyle(
@@ -21,6 +21,16 @@ class StartScreen extends StatelessWidget {
               fontSize: 24,
             ),
           ),
+          const SizedBox(height: 80),
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 219, 84, 6),
+            ),
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
         ],
       ),
     );
