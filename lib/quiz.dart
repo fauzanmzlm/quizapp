5e41ac35d096b5340cc2cd7e6f0af35de6916fc5
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-03-01 22:58:02 +0800

    commit

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,8 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/widgets.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,6 +14,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -27,7 +37,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
