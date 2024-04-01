ee0125dd23f6cf269ca7e66767d642ec89149a4b
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-04-01 13:52:27 +0800

    Managing The Questions Index As State

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -10,4 +10,94 @@ const questions = [
       'Functions',
     ],
   ),
+  QuizQuestion(
+    'What is the primary programming language used in Flutter development?',
+    [
+      'Dart',
+      'Java',
+      'Python',
+      'Swift',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget in Flutter is used to create a button?',
+    [
+      'RaisedButton',
+      'FlatButton',
+      'TextButton',
+      'IconButton',
+    ],
+  ),
+  QuizQuestion(
+    'What does the term "StatelessWidget" imply in Flutter?',
+    [
+      'It does not maintain any state',
+      'It maintains internal state',
+      'It is used for handling user input',
+      'It is only used in web applications',
+    ],
+  ),
+  QuizQuestion(
+    'Which command is used to create a new Flutter project from the command line interface (CLI)?',
+    [
+      'flutter create',
+      'flutter new',
+      'flutter start',
+      'flutter init',
+    ],
+  ),
+  QuizQuestion(
+    'What is the purpose of the "pubspec.yaml" file in a Flutter project?',
+    [
+      'It manages dependencies',
+      'It contains UI layout information',
+      'It stores user preferences',
+      'It controls app navigation',
+    ],
+  ),
+  QuizQuestion(
+    'What is the primary function of the "main.dart" file in a Flutter project?',
+    [
+      'It serves as the entry point for the application',
+      'It defines the overall app design',
+      'It handles user authentication',
+      'It contains only utility functions',
+    ],
+  ),
+  QuizQuestion(
+    'In Flutter, how can you add padding around a widget?',
+    [
+      'Using the Padding widget',
+      'Using the Margin widget',
+      'By setting the padding attribute directly on the widget',
+      'By adjusting the screen resolution',
+    ],
+  ),
+  QuizQuestion(
+    'What is the purpose of the "setState()" method in Flutter?',
+    [
+      'To notify the framework that the internal state of the widget has changed',
+      'To reset the entire application state',
+      'To initialize state variables',
+      'To force a rebuild of the entire widget tree',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget in Flutter is used to create a list of scrollable items?',
+    [
+      'ListView',
+      'GridView',
+      'Column',
+      'Row',
+    ],
+  ),
+  QuizQuestion(
+    'What is the purpose of the "hot reload" feature in Flutter development?',
+    [
+      'To quickly see changes made in the code without restarting the entire application',
+      'To optimize app performance',
+      'To deploy the application to multiple platforms simultaneously',
+      'To generate production-ready builds',
+    ],
+  ),
 ];
