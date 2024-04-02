d92e949a557ef0c02366956f6528b894c0f20774
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-04-02 18:56:11 +0800

    Using Third-Party Packages & Adding Google Fonts

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -16,10 +17,11 @@ class StartScreen extends StatelessWidget {
             width: 300,
             // color: const Color.fromARGB(135, 224, 224, 224),
           ),
-          const Text(
+          Text(
             'Learn flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 255, 255, 255),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontWeight: FontWeight.bold,
               fontSize: 24,
             ),
           ),
