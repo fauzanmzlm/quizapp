940223b5e13461bdc490b12fde44bc7042b98baa
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-04-05 12:41:37 +0800

    Expanded To The Rescue!

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -9,17 +9,19 @@ class QuestionsSummaary extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: summaryData.map((data) {
-        return Row(
+        return Row( 
           children: [
             Text(((data['question_index'] as int) + 1).toString()),
-            Column(children: [
-              Text(data['question'] as String),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(data['user_answer'] as String),
-              Text(data['correct_answer'] as String),
-            ])
+            Expanded(
+              child: Column(children: [
+                Text(data['question'] as String),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(data['user_answer'] as String),
+                Text(data['correct_answer'] as String),
+              ]),
+            )
           ],
         );
       }).toList(),
